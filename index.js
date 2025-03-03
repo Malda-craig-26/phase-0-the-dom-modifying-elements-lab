a8277bfacbca6b31dc745fbe0d555cfe141c40f0
kimberly <maldakimberly228@gmail.com>
2025-03-03 15:33:17 +0300

    creates a new file

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+
+document.querySelector("main#main").remove();
+
+
+const newHeader = document.createElement("h1");
+
+
+newHeader.id = "victory";
+
+
+newHeader.textContent = "Your-Name is the champion"; 
+
+
+document.body.appendChild(newHeader);
